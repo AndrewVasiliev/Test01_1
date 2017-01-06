8c336e9c09db9c422e5a662f3d39837865484566
AndrewVasiliev <vasiliev1979@yandex.ru>
2017-01-06 09:55:17 +0300

    hud (not finished) input (not finished)

--- core/src/com/andrewvasiliev/game/test01/MyGdxGame.java
+++ core/src/com/andrewvasiliev/game/test01/MyGdxGame.java
@@ -1,15 +1,22 @@
 package com.andrewvasiliev.game.test01;
 
+import com.andrewvasiliev.game.test01.Screens.MainMenu;
 import com.andrewvasiliev.game.test01.Screens.TestMainField;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class MyGdxGame extends Game {
     public int iWidthMeter, iHeightMeter;
+	public Screen gameScreen, mainMenu;
+    public Skin skin;
 
 
 	
@@ -20,7 +27,25 @@ public class MyGdxGame extends Game {
         iWidthMeter = 1600;
         iHeightMeter = 900;
 
-		this.setScreen(new TestMainField(this));
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("Roboto-Regular.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        //generator.DEFAULT_CHARS
+        parameter.characters += 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+        float ratio = Gdx.graphics.getWidth() / 960f;
+        parameter.size = 48 * (int)ratio;
+        BitmapFont skinFont = generator.generateFont(parameter);
+        //skin.addRegions(TextureAtlas(Gdx.files.internal("skin.atlas")));
+        //skin.add("normaltext", skinFont);
+        //skin.load(Gdx.files.internal("skin.json"))
+
+
+
+        gameScreen = new TestMainField(this);
+        mainMenu = new MainMenu(this);
+
+
+		this.setScreen(gameScreen);
 	}
 
 	@Override
@@ -30,5 +55,7 @@ public class MyGdxGame extends Game {
 	
 	@Override
 	public void dispose () {
+        super.dispose();
+        skin.dispose();
 	}
 }
---
+++ core/src/com/andrewvasiliev/game/test01/Screens/MainMenu.java
@@ -0,0 +1,53 @@
+package com.andrewvasiliev.game.test01.Screens;
+
+import com.andrewvasiliev.game.test01.MyGdxGame;
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by ava on 06.01.17.
+ */
+
+public class MainMenu implements Screen {
+    private MyGdxGame locGame;
+
+    public MainMenu(MyGdxGame myGdxGame) {
+        locGame = myGdxGame;
+
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
