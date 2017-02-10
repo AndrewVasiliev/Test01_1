81370a94e81820c7596ba1daf6c284a7c692bad1
AvA <vasiliev1979@yandex.ru>
2017-02-10 13:47:01 +0300

    начал меню старта игры 2

--- core/src/com/andrewvasiliev/game/test01/Screens/PreStartMenu.java
+++ core/src/com/andrewvasiliev/game/test01/Screens/PreStartMenu.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -22,11 +23,16 @@ public class PreStartMenu implements Screen {
     private MyGdxGame locGame;
     private Stage stage;
     private Table table;
-    private TextButton btnPvP, btnPvAI;
     public BackgroundActor backgroundActor;
     private ButtonGroup btnGroupGameType;
     private TextField edPlayer1, edPlayer2;
     private Label lblPlayer1, lblPlayer2;
+    private Label lblDifficulty;
+    private ButtonGroup btnGroupDifficulty;
+    private TextButton btnEasy, btnMid, btnHard;
+    private Label lblGameType;
+    //private TextButton btnPvP, btnPvAI;
+    private Button btnPvP, btnPvAI;
 
     public PreStartMenu(MyGdxGame myGdxGame) {
         locGame = myGdxGame;
@@ -37,8 +43,15 @@ public class PreStartMenu implements Screen {
         table. pad(30);
         //table.debugAll();
 
-        btnPvP = new CheckBox("Игрок против Игрока", locGame.skin, "default");
-        btnPvAI = new CheckBox("Игрок против Андроида", locGame.skin, "default");
+        //btnPvP = new CheckBox("Игрок против Игрока", locGame.skin, "default");
+        //btnPvAI = new CheckBox("Игрок против Андроида", locGame.skin, "default");
+
+        lblGameType = new Label("Игра против", locGame.skin, "default");
+        btnPvP = new Button(locGame.skin, "toggleButton");
+        btnPvP.add(new Label("Игрока", locGame.skin, "default"));
+        btnPvAI = new Button(locGame.skin, "toggleButton");
+        btnPvAI.add(new Label("Андроида", locGame.skin, "default"));
+
         btnGroupGameType = new ButtonGroup(btnPvP, btnPvAI);
         btnGroupGameType.setMaxCheckCount(1);
         btnGroupGameType.setMinCheckCount(1);
@@ -51,15 +64,39 @@ public class PreStartMenu implements Screen {
         edPlayer2 = new TextField("Игрок 2", locGame.skin, "default" );
         edPlayer2.setMaxLength(15);
 
+        lblDifficulty = new Label("Сложность Андроида", locGame.skin, "default");
+        btnEasy = new TextButton("Легкий", locGame.skin, "toggleButton");
+        btnMid = new TextButton("Средний", locGame.skin, "toggleButton");
+        btnHard= new TextButton("Тяжелый", locGame.skin, "toggleButton");
+        btnGroupDifficulty = new ButtonGroup(btnEasy, btnMid, btnHard);
+        btnGroupDifficulty.setMaxCheckCount(1);
+        btnGroupDifficulty.setMinCheckCount(1);
+        btnGroupDifficulty.setUncheckLast(true);
+
         float cnPad = 5;
-        table.add(btnPvP).pad(cnPad);
-        table.add(btnPvAI).pad(cnPad);
+        table.add();
+        table.add();
+        table.add();
+        table.add();
+        table.add();
         table.row();
+        //тип игры: PvP или PvAI
+        table.add(lblGameType).align(Align.right).pad(cnPad);
+        table.add(btnPvP).pad(cnPad).colspan(2);
+        table.add(btnPvAI).pad(cnPad).colspan(2);
+        table.row();
+        //имя игрока 1
         table.add(lblPlayer1).align(Align.right).pad(cnPad);
-        table.add(edPlayer1).align(Align.left).pad(cnPad);
+        table.add(edPlayer1).align(Align.left).pad(cnPad).colspan(4);
         table.row();
-        table.add(lblPlayer2).align(Align.right).pad(cnPad);
-        table.add(edPlayer2).align(Align.left).pad(cnPad);
+        //имя игрока 2
+        //table.add(lblPlayer2).align(Align.right).pad(cnPad);
+        //table.add(edPlayer2).align(Align.left).pad(cnPad);
+        //сложность Андроида
+        table.add(lblDifficulty).align(Align.right).pad(cnPad);
+        table.add(btnEasy).align(Align.left).pad(cnPad);
+        table.add(btnMid).align(Align.left).pad(cnPad);
+        table.add(btnHard).align(Align.left).pad(cnPad);
 
 
         backgroundActor = new BackgroundActor(locGame);
