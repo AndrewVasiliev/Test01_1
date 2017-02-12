9b117b3fd7ebd0092f5e2a4007f22b3f3f0a367b
Andrew <vasiliev1979@yandex.ru>
2017-02-12 13:00:30 +0300

    12.02.2017

--- core/src/com/andrewvasiliev/game/test01/Screens/PreStartMenu.java
+++ core/src/com/andrewvasiliev/game/test01/Screens/PreStartMenu.java
@@ -3,8 +3,11 @@ package com.andrewvasiliev.game.test01.Screens;
 import com.andrewvasiliev.game.test01.Actors.BackgroundActor;
 import com.andrewvasiliev.game.test01.MyGdxGame;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.TextInputListener;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
@@ -13,6 +16,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 
 /**
@@ -22,17 +26,18 @@ import com.badlogic.gdx.utils.Align;
 public class PreStartMenu implements Screen {
     private MyGdxGame locGame;
     private Stage stage;
-    private Table table;
+    private Table table, tblGameType, tblDifficulty, tblFieldType;
     public BackgroundActor backgroundActor;
     private ButtonGroup btnGroupGameType;
     private TextField edPlayer1, edPlayer2;
     private Label lblPlayer1, lblPlayer2;
-    private Label lblDifficulty;
+    private Label lblDifficulty, lblFieldType;
     private ButtonGroup btnGroupDifficulty;
-    private TextButton btnEasy, btnMid, btnHard;
+    private Button btnEasy, btnMid, btnHard;
     private Label lblGameType;
     //private TextButton btnPvP, btnPvAI;
     private Button btnPvP, btnPvAI;
+    private TextButton btnStartGame;
 
     public PreStartMenu(MyGdxGame myGdxGame) {
         locGame = myGdxGame;
@@ -40,8 +45,9 @@ public class PreStartMenu implements Screen {
 
         table = new Table ();
         table.setFillParent(true);
-        table. pad(30);
+        //table.pad(30);
         //table.debugAll();
+        float cnPad = 5;
 
         //btnPvP = new CheckBox("Игрок против Игрока", locGame.skin, "default");
         //btnPvAI = new CheckBox("Игрок против Андроида", locGame.skin, "default");
@@ -51,6 +57,9 @@ public class PreStartMenu implements Screen {
         btnPvP.add(new Label("Игрока", locGame.skin, "default"));
         btnPvAI = new Button(locGame.skin, "toggleButton");
         btnPvAI.add(new Label("Андроида", locGame.skin, "default"));
+        tblGameType = new Table();
+        tblGameType.add(btnPvP).pad(cnPad);
+        tblGameType.add(btnPvAI).pad(cnPad);
 
         btnGroupGameType = new ButtonGroup(btnPvP, btnPvAI);
         btnGroupGameType.setMaxCheckCount(1);
@@ -60,44 +69,85 @@ public class PreStartMenu implements Screen {
         lblPlayer1 = new Label("Имя игрока 1", locGame.skin, "default");
         edPlayer1 = new TextField("Игрок 1", locGame.skin, "default" );
         edPlayer1.setMaxLength(15);
+
+
+/*
+        edPlayer1.setOnscreenKeyboard(new TextField.OnscreenKeyboard() {
+            @Override
+            public void show(boolean visible) {
+                //Gdx.input.setOnscreenKeyboardVisible(true);
+                Gdx.input.getTextInput(new Input.TextInputListener() {
+                    @Override
+                    public void input(String text) {
+                        edPlayer1.setText(text);
+                    }
+
+                    @Override
+                    public void canceled() {
+
+                    }
+                }, lblPlayer1.getText().toString(), edPlayer1.getText(), "");
+            }
+        });
+*/
+
         lblPlayer2 = new Label("Имя игрока 2", locGame.skin, "default");
         edPlayer2 = new TextField("Игрок 2", locGame.skin, "default" );
         edPlayer2.setMaxLength(15);
 
         lblDifficulty = new Label("Сложность Андроида", locGame.skin, "default");
-        btnEasy = new TextButton("Легкий", locGame.skin, "toggleButton");
-        btnMid = new TextButton("Средний", locGame.skin, "toggleButton");
-        btnHard= new TextButton("Тяжелый", locGame.skin, "toggleButton");
+        btnEasy = new Button(locGame.skin, "toggleButton");
+        btnEasy.add(new Label("Легкий", locGame.skin, "default"));
+        btnMid = new Button(locGame.skin, "toggleButton");
+        btnMid.add(new Label("Средний", locGame.skin, "default"));
+        btnHard= new Button(locGame.skin, "toggleButton");
+        btnHard.add(new Label("Тяжелый", locGame.skin, "default"));
         btnGroupDifficulty = new ButtonGroup(btnEasy, btnMid, btnHard);
         btnGroupDifficulty.setMaxCheckCount(1);
         btnGroupDifficulty.setMinCheckCount(1);
         btnGroupDifficulty.setUncheckLast(true);
+        tblDifficulty = new Table();
+        tblDifficulty.add(btnEasy).align(Align.left).pad(cnPad);
+        tblDifficulty.add(btnMid).align(Align.left).pad(cnPad);
+        tblDifficulty.add(btnHard).align(Align.left).pad(cnPad);
 
-        float cnPad = 5;
-        table.add();
-        table.add();
-        table.add();
-        table.add();
-        table.add();
-        table.row();
+        lblFieldType = new Label("Тип ячеек", locGame.skin, "default");
+        //.. тип ячеек
+        tblFieldType = new Table();
+
+        btnStartGame = new TextButton("Начать игру", locGame.skin, "default");
+
+
+
+
+
+
         //тип игры: PvP или PvAI
         table.add(lblGameType).align(Align.right).pad(cnPad);
-        table.add(btnPvP).pad(cnPad).colspan(2);
-        table.add(btnPvAI).pad(cnPad).colspan(2);
+        table.add(tblGameType).align(Align.left);
         table.row();
         //имя игрока 1
         table.add(lblPlayer1).align(Align.right).pad(cnPad);
-        table.add(edPlayer1).align(Align.left).pad(cnPad).colspan(4);
+        table.add(edPlayer1).align(Align.left).pad(cnPad);
         table.row();
         //имя игрока 2
-        //table.add(lblPlayer2).align(Align.right).pad(cnPad);
-        //table.add(edPlayer2).align(Align.left).pad(cnPad);
+        table.add(lblPlayer2).align(Align.right).pad(cnPad);
+        table.add(edPlayer2).align(Align.left).pad(cnPad);
+        table.row();
         //сложность Андроида
         table.add(lblDifficulty).align(Align.right).pad(cnPad);
-        table.add(btnEasy).align(Align.left).pad(cnPad);
-        table.add(btnMid).align(Align.left).pad(cnPad);
-        table.add(btnHard).align(Align.left).pad(cnPad);
+        table.add(tblDifficulty);
+        table.row();
+        //тип ячеек
+        table.add(lblFieldType).align(Align.right).pad(cnPad);
+        table.add(tblFieldType);
+        table.row();
+        //кнопка начать игру
+        table.add(btnStartGame).align(Align.center).padTop(20).colspan(2);
 
+
+        btnPvP.setChecked(true);
+        btnEasy.setChecked(true);
 
         backgroundActor = new BackgroundActor(locGame);
         backgroundActor.setPosition(0, 0);
